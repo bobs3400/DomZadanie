33b0a8cf3ba0a9ecfc087ead9962c5b75b38078a
Роберт <bobs3400@mail.ru>
2023-02-10 21:17:07 +1000

    создали программу квадрат числа4

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите 1 целое число");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите 2 целое число");
+int numberB = int.Parse(Console.ReadLine());
+if(numberA > numberB)
+{
+    Console.WriteLine("Наибольшее число  " + numberA);
+    Console.WriteLine("Наименьшее число  " + numberB);
+}
+else
+{
+   Console.WriteLine("Наибольшее число  " + numberB);
+    Console.WriteLine("Наименьшее число  " + numberA); 
+}
